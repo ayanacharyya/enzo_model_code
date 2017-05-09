4913d55e252d60ed0db419893d9c44646f35ebdf
Ayan Acharyya <acharyyaayan@gmail.com>
2017-05-10 04:39:41 +1000

    added *realisation* tag to file names

--- loopcall.py
+++ loopcall.py
@@ -2,14 +2,37 @@
 #by Ayan
 import subprocess
 import sys
+import numpy as np
+import os
+HOME = os.getenv('HOME')+'/'
+import time
+start_time= time.time()
 
 function = 'plotobservables.py'
-arg_arr = [10,20,30,50,70,100,120,150] #[150,200,300,400,500,600,700,800] #arg = vres
-snr = 5.
-for arg in arg_arr:
-    filename = 'PPV_DD0600_lgfOm=0.5,res=0.4kpc_6400.0-6782.674A_specsmeared_'+str(arg)+'kmps_smeared_moff_parm5,4.7,51_noisy_obs.fits'
-    subprocess.call(['python '+function+' --fitsname '+filename+' --addnoise --smooth \
-    --spec_smear --vres '+str(arg)+' --wmin 6400 --met --snr '+str(snr)+' --saveplot']\
-    ,shell=True)
-    
-print 'Finished looping!'
+snr_arr = np.arange(11)
+logOHcen = 8.77
+logOHgrad_arr = [-0.05]#[-0.1,-0.05,-0.025,-0.01] #arg = logOHgrad
+scale_exptime_arr = [600]#,1200] #exp time for arc=0.5, pivoting which the exp time for other arc values would be determined
+file_arr = ['DD0600_lgf']#,'DD0600']
+#multi_arr = np.arange(10)+1 #different realisations
+vres_arr = [10,20,30,50,70,90] #vres
+arc_arr = [0.1,0.5,0.8,1.0,1.5,2.0,2.5,3.0] #arg = res_arcsec
+outpath = HOME+'Desktop/bpt/'
+#fixed_SNR = 5.
+for file in file_arr:
+    for scale_exptime in scale_exptime_arr:
+        for logOHgrad in logOHgrad_arr:
+            for snr in snr_arr:
+                arc = 0.5
+                for vres in vres_arr:
+                    subprocess.call(['python '+function+' --addnoise --smooth --spec_smear \
+--vres '+str(vres)+' --wmin 6400 --Zgrad '+str(logOHcen)+','+str(logOHgrad)+' --met --snr '+str(snr)+' --calc --hide --keep \
+--arc '+str(arc)+' --scale_exptime '+str(scale_exptime)+' --file '+file+' --path '+outpath],shell=True)
+                
+                vres = 30
+                for arc in arc_arr:
+                    subprocess.call(['python '+function+' --addnoise --smooth --spec_smear \
+--vres '+str(vres)+' --wmin 6400 --Zgrad '+str(logOHcen)+','+str(logOHgrad)+' --met --snr '+str(snr)+' --calc --hide --keep \
+--arc '+str(arc)+' --scale_exptime '+str(scale_exptime)+' --file '+file+' --path '+outpath],shell=True)
+
+print 'Finished looping in %s minutes!'% ((time.time() - start_time)/60)
